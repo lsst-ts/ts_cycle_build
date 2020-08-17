c2f3d6a002efa44c3c72650d891c1a17d381938f
Tiago Ribeiro <tiago.astro@gmail.com>
2020-08-17 15:24:42 -0700

    Update deployment environment to source the entrypoint instead of running a CMD. Update startup script to catch term signal and properly finalize tasks. Add test jenkinsfile. Fix M1M3 userid. Add watcher. Update sal and salobj versions to use the experimental version. Update Cycle build. Update README with a version history. In `lsstsqre/Dockerfile` stop installing community edition to get `idlpp`. We now have a license that allow us to run it. In `lsstsqre/setup.sh` fix path to setup_sal_env. Implement scriptqueue Dockerfile. Override Ptg configurations with appropriate command responses.

---
+++ build/atmcs_sim/startup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+source ${HOME}/.setup_sal_env.sh
+
+echo "# Starting ATMCS Simulator CSC"
+
+run_atmcs_simulator.py &
+
+pid="$!"
+
+wait ${pid}
--- build/deploy-env/conda/setup_sal_env.sh
+++ build/deploy-env/conda/setup_sal_env.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
 . ${WORKDIR}/miniconda/bin/activate
 
-source ${HOME}/ospl_env.sh
+source ${HOME}/.ospl_env.sh
+
+# setup handlers
+# on callback, kill the last background process and execute term_handler
+trap 'kill ${!}; term_handler' SIGTERM
 
 source ${OSPL_HOME}/release.com
-
-export PATH=${WORKDIR}/bin:${PATH}
