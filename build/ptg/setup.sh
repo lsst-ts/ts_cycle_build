2be691f211b8ce71a44f1ff5588bdd63a016567f
Tiago Ribeiro <tiago.astro@gmail.com>
2020-09-01 11:21:40 -0700

    Update ScriptQueue version. Updatet ts_config_attcs version. Update ts_athexapod version. Add handle for exit code in queue and watcher. Improve ptg entrypoint.
    Added Jenkinsfile.broker to act as a single job to trigger all the CSC Conda builds.

--- build/ptg/setup.sh
+++ build/ptg/setup.sh
@@ -8,8 +8,11 @@ pid=0
 # SIGTERM-handler
 term_handler() {
   if [ $pid -ne 0 ]; then
+    echo Sending SIGTERM to $pid
     kill -SIGTERM "$pid"
     wait "$pid"
+    exit_code=$?
+    echo $pid exited with code ${exit_code}
   fi
   exit 143; # 128 + 15 -- SIGTERM
 }
@@ -19,12 +22,12 @@ trap 'kill ${!}; term_handler' SIGTERM
 if [ "$TELESCOPE" == "MT" ]
 then
     echo "# Running main telescope pointing component..."
-    /home/saluser/repos/ts_pointing_common/build/bin/MTPtgController &
+    /home/saluser/repos/ts_pointing_common/build/bin/MTPtgController &> /dev/null &
     pid="$!"
 elif [ "$TELESCOPE" == "AT" ]
 then
     echo "# Running auxiliary telescope pointing component..."
-    /home/saluser/repos/ts_pointing_common/build/bin/ATPtgController &
+    /home/saluser/repos/ts_pointing_common/build/bin/ATPtgController &> /dev/null &
     pid="$!"
 else
     echo "# Unrecognized telescope selection ${TELESCOPE}. Must be one of MT or AT"
@@ -32,3 +35,9 @@ else
 fi
 
 wait ${pid}
+
+exit_code=$?
+
+echo ${TELESCOPE} ptg exited with code ${exit_code}
+
+exit 0
