11533508b0696bcd51a5903f8060bd60248b673d
tribeiro <tribeiro@lsst.org>
2021-01-25 13:51:19 -0700

    Implement patches to scriptqueue to do a quick build.

--- build/scriptqueue/startup.sh
+++ build/scriptqueue/startup.sh
@@ -2,6 +2,13 @@
 
 source ${WORKDIR}/.setup_sal_env.sh
 
+setup cwfs -t current
+setup lsst_distrib
+setup obs_base -t $USER
+setup pipe_tasks -t $USER
+setup rapid_analysis -t $USER
+setup atmospec -t $USER
+setup ts_observing_utilities -t $USER
 setup ts_scriptqueue
 
 run_script_queue.py ${RUN_ARG} &
