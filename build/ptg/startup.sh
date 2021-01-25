ce1c39f90de1301d334bbedefd49b9b685c3d21c
Tiago Ribeiro <tiago.astro@gmail.com>
2021-01-25 15:54:14 -0700

    Update lsstsqre build to receive ts_idl version Update version of the lsstsqre stack to w_2020_02. Update pointing component to v2.4.0 and build scripts. Update MTAOS version and build script. This latest version does not require lsst_sims anymore. Update ScriptQueue build with additional dependencies for CWFS and rapid analysis scripts. Update Scheduler version. Update version of ts_m2 package. Update version of ts_atspec package.

--- build/ptg/startup.sh
+++ build/ptg/startup.sh
@@ -5,12 +5,12 @@ source ${HOME}/.setup_sal_env.sh
 if [ "$TELESCOPE" == "MT" ]
 then
     echo "# Running main telescope pointing component..."
-    /home/saluser/repos/ts_pointing_common/build/bin/MTPtgController &> /dev/null &
+    /home/saluser/repos/ts_pointing_common/build/bin/MTPtgController &
     pid="$!"
 elif [ "$TELESCOPE" == "AT" ]
 then
     echo "# Running auxiliary telescope pointing component..."
-    /home/saluser/repos/ts_pointing_common/build/bin/ATPtgController &> /dev/null &
+    /home/saluser/repos/ts_pointing_common/build/bin/ATPtgController &
     pid="$!"
 else
     echo "# Unrecognized telescope selection ${TELESCOPE}. Must be one of MT or AT"
@@ -18,9 +18,3 @@ else
 fi
 
 wait ${pid}
-
-exit_code=$?
-
-echo ${TELESCOPE}Ptg exited with code ${exit_code}
-
-exit 0
