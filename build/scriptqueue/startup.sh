8eb5e33e500f22e64143c562d17f529d776c3433
Tiago Ribeiro <tiago.astro@gmail.com>
2021-02-16 15:31:57 -0700

    DM-28826: Support AT observing run Cycle 0017 Revision 002: Updates done during the AT run SUMMIT-4829.   * Fix script queue build.   * Update version of ts_observatory_control   * Update version of ts_ataos   * Update version of obs_base   * Update version of pipe_tasks   * Update version of atmospec   * Update version of ts_observing_utilities   * Update version of ts_config_attcs General fixes to develop-env build.   * Install ltd-conveyor using pip.

--- build/scriptqueue/startup.sh
+++ build/scriptqueue/startup.sh
@@ -2,6 +2,16 @@
 
 source ${WORKDIR}/.setup_sal_env.sh
 
+setup lsst_distrib
+setup cwfs -t current
+setup obs_base -t $USER
+setup pipe_tasks -t $USER
+setup rapid_analysis -t $USER
+setup atmospec -t $USER
+setup ts_observing_utilities -t $USER
+setup ts_observatory_control
+setup ts_externalscripts
+setup ts_standardscripts
 setup ts_scriptqueue
 
 run_script_queue.py ${RUN_ARG} &
