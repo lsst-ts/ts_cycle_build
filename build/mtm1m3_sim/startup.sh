0d3ad52f54f267a8473c790ac9664c2a67e929be
Tiago Ribeiro <tiago.astro@gmail.com>
2021-04-13 12:16:34 -0700

    Update m1m3_sim build script and startup file to accept a `RUN_ARG` environment variable to control arguments.

--- build/mtm1m3_sim/startup.sh
+++ build/mtm1m3_sim/startup.sh
@@ -3,7 +3,7 @@
 source ${HOME}/.setup_sal_env.sh
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/lsst/ts_sal/lib/
 
-/home/saluser/ts_m1m3support/ts_M1M3Support -d -c /home/saluser/ts_m1m3support/SettingFiles &
+/home/saluser/ts_m1m3support/ts_M1M3Support ${RUN_ARG} -c /home/saluser/ts_m1m3support/SettingFiles &
 
 pid="$!"
 
