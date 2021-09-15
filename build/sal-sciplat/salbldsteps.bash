784acae7242c77a7f1289ce7774c3460b5f15d6e
Tiago Ribeiro <tribeiro@lsst.org>
2021-09-15 15:53:00 -0700

    Update sal-sciplat build to use new `opensplice/lsstsqre` intermediate stage containers. Update compose file to build new `opensplice/lsstsqre` and sal-sciplat`.

--- build/sal-sciplat/salbldsteps.bash
+++ build/sal-sciplat/salbldsteps.bash
@@ -2,7 +2,11 @@
 source ${LOADSTACK}
 unset LD_PRELOAD
 setup lsst_distrib
+setup ts_salobj -t current
+setup ts_observatory_control -t current
+setup ts_standardscripts -t current
+setup ts_externalscripts -t current
 
 source ${OSPL_HOME}/release.com
 
-export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-sp.xml').as_uri())")
+export OSPL_URI=$(python -c "from lsst.ts import ddsconfig; print( (ddsconfig.get_config_dir() / 'ospl-shmem.xml').as_uri())")
