81edfa862b65ea42a1c597f6385fe29b1734c7bb
Michael Reuter <mareuter@lsst.org>
2021-10-05 10:11:59 -0700

    Updates for rubintv-broadcaster.

--- build/rubintv-broadcaster/startup.sh
+++ build/rubintv-broadcaster/startup.sh
@@ -1,6 +1,20 @@
 
 source ${WORKDIR}/loadLSST.bash
 
+# Perform updates to all repos before script launch
+# We start in rapid_analysis/scripts and need to return there
+cd ../
+directories="Spectractor atmospec rapid_analysis"
+for directory in ${directories}
+do
+  cd ../${directory}
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  git fetch --all
+  git checkout ${branch}
+  git reset --hard origin/${branch}
+done
+cd scripts
+
 setup lsst_distrib
 setup atmospec -j -t current
 setup rapid_analysis -j -t current 
