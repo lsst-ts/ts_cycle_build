ae1c190d865cfe11258e4d7b5bc419a21c85e1e4
Tiago Ribeiro <tribeiro@lsst.org>
2023-01-11 08:56:19 -0700

    In ``rubintv-broadcaster``, add new dependency ``eo_pipe``.

--- build/rubintv-broadcaster/startup.sh
+++ build/rubintv-broadcaster/startup.sh
@@ -1,10 +1,12 @@
 
 source ${WORKDIR}/loadLSST.bash
 
+umask 002
+
 # Perform updates to all repos before script launch
 # We start in rubintv_production/scripts and need to return there
 cd ../
-directories="Spectractor atmospec summit_utils summit_extras rubintv_production"
+directories="Spectractor atmospec summit_utils summit_extras rubintv_production eo_pipe"
 for directory in ${directories}
 do
   cd ../${directory}
@@ -12,7 +14,8 @@ do
   git fetch --all
   git checkout ${DEPLOY_BRANCH} && (git reset --hard origin/${DEPLOY_BRANCH}) || (git checkout ${branch} && git reset --hard origin/${branch})
 done
-cd scripts
+
+cd ../rubintv_production/scripts
 
 setup lsst_distrib
 setup spectractor -t saluser
@@ -20,5 +23,6 @@ setup atmospec -j -t saluser
 setup summit_utils -j -t saluser
 setup summit_extras -j -t saluser
 setup rubintv_production -j -t saluser
+setup eo_pipe -j -t saluser
 
 python $RUN_ARG
