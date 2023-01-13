e3967025d0885bdaf9a3c0c4067bb794380b0b2f
Tiago Ribeiro <tribeiro@lsst.org>
2023-01-13 09:51:03 -0700

    In rubintv-broadcaster, refactor build script to move repositories to /repos.

--- build/rubintv-broadcaster/startup.sh
+++ build/rubintv-broadcaster/startup.sh
@@ -1,7 +1,8 @@
 
-source ${WORKDIR}/loadLSST.bash
 
 umask 002
+
+git config --global --add safe.directory '*'
 
 # Perform updates to all repos before script launch
 # We start in rubintv_production/scripts and need to return there
@@ -9,13 +10,15 @@ cd ../
 directories="Spectractor atmospec summit_utils summit_extras rubintv_production eo_pipe"
 for directory in ${directories}
 do
-  cd ../${directory}
+  cd /repos/${directory}
   branch=$(git rev-parse --abbrev-ref HEAD)
   git fetch --all
   git checkout ${DEPLOY_BRANCH} && (git reset --hard origin/${DEPLOY_BRANCH}) || (git checkout ${branch} && git reset --hard origin/${branch})
 done
 
-cd ../rubintv_production/scripts
+cd /repos/rubintv_production/scripts
+
+source ${WORKDIR}/loadLSST.bash
 
 setup lsst_distrib
 setup spectractor -t saluser
