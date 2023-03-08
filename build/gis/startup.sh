da9d11768fcfcea1918a74523e02fd1d316f99f7
Eric Coughlin <ecoughlin@lsst.org>
2023-03-08 16:23:42 -0300

    Move the copy command to the startup script

--- build/gis/startup.sh
+++ build/gis/startup.sh
@@ -2,6 +2,10 @@
 
 source $HOME/.setup_sal_env.sh
 
+# This is necessary to add here because the store folder is only available at deploy time and not at build time.
+mkdir /home/saluser/.ssh && \
+cp /home/saluser/.store/* /home/saluser/.ssh/ # Put the patched ssh keys into the ssh folder
+
 run_gis $RUN_ARG &
 
 pid="$!"
