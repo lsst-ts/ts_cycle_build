f4a44ecc90d9132b77ed0a11bf667a3216c0be89
Eric Coughlin <ecoughlin@lsst.org>
2023-03-09 10:59:22 -0300

    Ensure .ssh folder for GIS is 700

--- build/gis/startup.sh
+++ build/gis/startup.sh
@@ -3,7 +3,7 @@
 source $HOME/.setup_sal_env.sh
 
 # This is necessary to add here because the store folder is only available at deploy time and not at build time.
-mkdir /home/saluser/.ssh && \
+mkdir -m 700 /home/saluser/.ssh && \
 cp /home/saluser/.store/* /home/saluser/.ssh/ # Put the patched ssh keys into the ssh folder
 
 run_gis $RUN_ARG &
