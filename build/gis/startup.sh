b9ed781d81cc1f13019bc3bce35ef2ff60826fb1
Tiago Ribeiro <tribeiro@lsst.org>
2023-03-14 14:06:43 -0700

    Add build scripts for GIS.

---
+++ build/gis/startup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source $HOME/.setup_sal_env.sh
+
+# This is necessary to add here because the store folder is only available at deploy time and not at build time.
+mkdir -m 700 /home/saluser/.ssh && \
+cp /home/saluser/.store/* /home/saluser/.ssh/ # Put the patched ssh keys into the ssh folder
+
+run_gis $RUN_ARG &
+
+pid="$!"
+
+wait ${pid}
