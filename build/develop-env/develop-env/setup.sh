2dc574ee6c6e32d55eaaabff359f974c93e1b230
Wouter van Reeven <wvanreeven@lsst.org>
2023-07-12 10:15:13 +0200

    In develop-env setup.sh, replace reference to ts_cRIOpy to ts_criopy.

--- build/develop-env/develop-env/setup.sh
+++ build/develop-env/develop-env/setup.sh
@@ -17,7 +17,7 @@ setup ts_config_ocs -t current
 setup ts_externalscripts -t current
 setup ts_hexrotcomm -t current
 setup ts_idl -t current
-setup ts_cRIOpy -t current
+setup ts_criopy -t current
 setup ts_observatory_control -t current
 setup ts_sal -t current
 setup ts_salobj -t current
