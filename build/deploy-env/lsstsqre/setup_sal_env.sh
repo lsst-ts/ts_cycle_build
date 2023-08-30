e123ab801dfca8d87bdfa1d0caf9817f36e461c6
Tiago Ribeiro <tribeiro@lsst.org>
2023-08-30 13:59:38 -0700

    Update deploy env lsstsqre build script to remove DDS dependency.

--- build/deploy-env/lsstsqre/setup_sal_env.sh
+++ build/deploy-env/lsstsqre/setup_sal_env.sh
@@ -13,16 +13,6 @@ term_handler() {
 
 . ${WORKDIR}/loadLSST.bash
 
-source ${WORKDIR}/ospl_env.sh
-
-source ${OSPL_HOME}/release.com
-
-ospl_release_string=$(ospl -v)
-ospl_release_pattern='OpenSplice version : (.*)'
-[[ "$ospl_release_string" =~ $ospl_release_pattern ]]
-
-export OSPL_RELEASE=${BASH_REMATCH[1]}
-
 # setup handlers
 # on callback, kill the last background process and execute term_handler
 trap 'kill ${!}; term_handler' SIGTERM
