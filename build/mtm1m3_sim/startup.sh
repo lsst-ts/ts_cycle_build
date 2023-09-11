5b069ba2e3cad2b2ab06320a8f8b9ff45e98fcc3
Tiago Ribeiro <tribeiro@lsst.org>
2023-09-11 10:53:55 -0700

    DELETE ME: Making m1m3 build work with kafka version of SAL.

--- build/mtm1m3_sim/startup.sh
+++ build/mtm1m3_sim/startup.sh
@@ -3,8 +3,9 @@
 source ${HOME}/.setup_sal_env.sh
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/lsst/ts_sal/lib/
 
-/home/saluser/ts_m1m3support/ts-M1M3supportd ${RUN_ARG} -c /home/saluser/ts_m1m3support/SettingFiles &
-
-pid="$!"
-
-wait ${pid}
+# /home/saluser/ts_m1m3support/ts-M1M3supportd ${RUN_ARG} -c /home/saluser/ts_m1m3support/SettingFiles &
+# 
+# pid="$!"
+# 
+# wait ${pid}
+/bin/bash
