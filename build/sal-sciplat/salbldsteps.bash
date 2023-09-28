98b44335ab77f2fa5cc52d3e93e02b7245dbdce9
Michael Reuter <mareuter@lsst.org>
2023-09-28 13:20:22 -0700

    Add Kafka envvars to sal-sciplat salbldsteps.sh script.

--- build/sal-sciplat/salbldsteps.bash
+++ build/sal-sciplat/salbldsteps.bash
@@ -6,3 +6,6 @@ setup ts_salobj -t current
 setup ts_observatory_control -t current
 setup ts_standardscripts -t current
 setup ts_externalscripts -t current
+
+export LSST_KAFKA_SECURITY_USERNAME=$(cat ${LSST_KAFKA_PASSFILE} | cut -d: -f1)
+export LSST_KAFKA_SECURITY_PASSWORD=$(cat ${LSST_KAFKA_PASSFILE} | cut -d: -f2)
