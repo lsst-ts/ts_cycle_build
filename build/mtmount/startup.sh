f8e353bf84db9a07498bad22a65905db7017af57
Tiago Ribeiro <tribeiro@lsst.org>
2024-01-30 14:50:23 -0700

    Update mtmount startup script to support starting the CSC in CCW only mode.

--- build/mtmount/startup.sh
+++ build/mtmount/startup.sh
@@ -2,8 +2,13 @@
 
 source $HOME/.setup_sal_env.sh
 
-echo "# Starting MTMount CSC"
-run_mtmount $RUN_ARG &
+if [[ -z "${CCW_ONLY}" ]]; then
+  echo "# Starting MTMount CSC in CCW only mode."
+  run_mtmount_ccw_only $RUN_ARG &
+else
+  echo "# Starting MTMount CSC."
+  run_mtmount $RUN_ARG &
+fi
 
 pid="$!"
 
