3d3e85eca8296cc90b005ed00b03fa4ce2a3d44e
Tiago Ribeiro <tribeiro@lsst.org>
2024-02-27 17:36:07 -0300

    DELETE ME: M1M3 MAKE DRY RUN

--- build/mtm1m3_sim/startup.sh
+++ build/mtm1m3_sim/startup.sh
@@ -3,9 +3,8 @@
 source ${HOME}/.setup_sal_env.sh
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/lsst/ts_sal/lib/
 
-# /home/saluser/ts_m1m3support/ts-M1M3supportd ${RUN_ARG} -c /home/saluser/ts_m1m3support/SettingFiles &
-# 
-# pid="$!"
-# 
-# wait ${pid}
-/bin/bash
+/home/saluser/ts_m1m3support/ts-M1M3supportd ${RUN_ARG} -c /home/saluser/ts_m1m3support/SettingFiles &
+
+pid="$!"
+
+wait ${pid}
