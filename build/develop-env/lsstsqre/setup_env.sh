24989a8acf15a6a4ef7206ac706b07f661d05809
Wouter van Reeven <wvanreeven@lsst.org>
2024-03-15 21:59:20 +0100

    Export PySide2 env vars for the develop environment.

--- build/develop-env/lsstsqre/setup_env.sh
+++ build/develop-env/lsstsqre/setup_env.sh
@@ -31,8 +31,12 @@ export LSST_DDS_IP=`ip route get 1 | awk '{print $7;exit}'`
 # Needed for Python-GPhoto2
 export LDFLAGS="-L/usr/local/lib -L/usr/local/lib/libgphoto2/${gphoto2} -L/usr/local/lib/libgphoto2_port/0.12.0"
 
-# Suppress several an OpenGL warning
+# Suppress several OpenGL warnings
 export LIBGL_ALWAYS_INDIRECT=1
+
+# Needed for PySide2
+export QT_API="PySide2"
+export PYTEST_QT_API="PySide2"
 
 export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:$CONDA_PREFIX/share/pkgconfig:$CONDA_PREFIX/lib/pkgconfig
 
