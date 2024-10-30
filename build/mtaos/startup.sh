27b1559b895f9dddcb3dcc1285fa051ba0800f19
Tiago Ribeiro <tribeiro@lsst.org>
2024-10-30 16:12:27 -0700

    Update mtaos startup script to use configuration from obs-env, if it is available.

--- build/mtaos/startup.sh
+++ build/mtaos/startup.sh
@@ -14,6 +14,13 @@ setup ts_ofc -t current
 setup ts_phosim -t current
 setup ts_mtaos -t current
 
+if [[ -d "/net/obs-env/auto_base_packages/ts_config_mttcs" ]]; then
+    export TS_CONFIG_MTTCS_DIR=/net/obs-env/auto_base_packages/ts_config_mttcs
+    echo "Using obs-env MTTCS configuration @ ${TS_CONFIG_MTTCS_DIR}."
+else
+    echo "Using standard MTTCS configuration @ ${TS_CONFIG_MTTCS_DIR}."
+fi
+
 run_mtaos $RUN_ARG &
 
 pid="$!"
