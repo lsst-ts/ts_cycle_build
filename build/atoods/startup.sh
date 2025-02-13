a68a03b43d49d308cecfeef70be2f5f9b7e11778
Michael Reuter <mareuter@lsst.org>
2025-02-13 12:00:25 -0700

    Remove unset S3_ENDPOINT_URL for oods builds.

--- build/atoods/startup.sh
+++ build/atoods/startup.sh
@@ -2,8 +2,6 @@
 
 source ${HOME}/.setup_sal_env.sh
 
-unset S3_ENDPOINT_URL
-
 setup lsst_distrib
 setup -t current ctrl_oods
 
--- build/oods/startup.sh
+++ build/oods/startup.sh
@@ -3,7 +3,6 @@
 source ${HOME}/.setup_sal_env.sh
 
 LSST_USER_HOME=/home/saluser
-unset S3_ENDPOINT_URL
 
 setup lsst_distrib
 setup obs_lsst
