8ffe34ff9780157f97e03dec93d587160ead7b2f
Tiago Ribeiro <tribeiro@lsst.org>
2025-02-24 10:36:03 -0700

    Update m1m3 to use gcc toolset 11.

--- build/mtm1m3_sim/startup.sh
+++ build/mtm1m3_sim/startup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 source ${HOME}/.setup_sal_env.sh
+source ${HOME}/.sal_setup.sh
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/lsst/ts_sal/lib/
 
 /home/saluser/ts_m1m3support/ts-M1M3supportd ${RUN_ARG} -c /home/saluser/ts_m1m3support/SettingFiles &
