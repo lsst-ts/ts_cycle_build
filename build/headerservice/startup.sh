bb2f4a4b9d2792d74dc2e4134006d7aba9c085ae
Tiago Ribeiro <tribeiro@lsst.org>
2025-04-09 22:09:02 -0400

    Update Header Service startup script to pass the RUN_ARG environment variable.

--- build/headerservice/startup.sh
+++ build/headerservice/startup.sh
@@ -4,7 +4,7 @@ source $HOME/.setup_sal_env.sh
 
 source $CONDA_PREFIX/HeaderService/setpath.sh $CONDA_PREFIX/HeaderService
 
-headerservice -c $HEADERSERVICE_DIR/etc/conf/${CAMERA}Telemetry.yaml $PORT_SPEC $URL_SPEC &
+headerservice -c $HEADERSERVICE_DIR/etc/conf/${CAMERA}Telemetry.yaml $PORT_SPEC $URL_SPEC $RUN_ARG &
 
 pid="$!"
 
