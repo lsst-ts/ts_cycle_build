4eb1cef24f922c120cce6432e0fab64eb9365e19
Tiago Ribeiro <tribeiro@lsst.org>
2025-06-11 16:49:12 -0700

    fixup! Add build scripts for rubin ToO producer.

--- build/rubin_too_producer/startup.sh
+++ build/rubin_too_producer/startup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-printenv
+file_path="${HOME}/.config/hop/auth.toml"  # Replace with the actual file path
+
+while [ ! -f "$file_path" ]; do
+  echo "File '$file_path' does not exist yet. Sleeping..."
+  sleep 5  # Sleep for 5 seconds, adjust as needed
+done
 
 python3.12 /usr/src/rubin-ToO-producer/forward_alerts.py -f ${HOME}/config.yaml
 
