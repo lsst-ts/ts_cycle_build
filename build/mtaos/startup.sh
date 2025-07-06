8c5dcbf288fe18cbd1c525ac0c6b49b46fe6a607
Tiago Ribeiro <tribeiro@lsst.org>
2025-07-05 23:00:27 -0400

    Update mtaos startup script to add the configuration directory to git safe directory to allow the CSC to run git describe.

--- build/mtaos/startup.sh
+++ build/mtaos/startup.sh
@@ -17,6 +17,7 @@ setup ts_mtaos -t current
 
 if [[ -d "/net/obs-env/auto_base_packages/ts_config_mttcs" ]]; then
     export TS_CONFIG_MTTCS_DIR=/net/obs-env/auto_base_packages/ts_config_mttcs
+    git config --global --add safe.directory ${TS_CONFIG_MTTCS_DIR}
     echo "Using obs-env MTTCS configuration @ ${TS_CONFIG_MTTCS_DIR}."
 else
     echo "Using standard MTTCS configuration @ ${TS_CONFIG_MTTCS_DIR}."
