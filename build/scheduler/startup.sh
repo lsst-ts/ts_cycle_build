9ecae573a5e5cf6b6cd4ad2ee5a0825de436085d
Tiago Ribeiro <tribeiro@lsst.org>
2025-07-05 23:01:06 -0400

    Update scheduler startup script to add the configuration directory to git safe directory to allow the CSC to run git describe.

--- build/scheduler/startup.sh
+++ build/scheduler/startup.sh
@@ -4,6 +4,7 @@ source $HOME/.setup_sal_env.sh
 
 if [[ -d "/net/obs-env/auto_base_packages/ts_config_ocs" ]]; then
     export TS_CONFIG_OCS_DIR=/net/obs-env/auto_base_packages/ts_config_ocs
+    git config --global --add safe.directory ${TS_CONFIG_OCS_DIR}
     echo "Using obs-env Scheduler configuration @ ${TS_CONFIG_OCS_DIR}."
 else
     echo "Using standard Scheduler configuration @ ${TS_CONFIG_OCS_DIR}."
