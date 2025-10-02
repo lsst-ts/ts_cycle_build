270c96fbfb8b5a5be141739556b3d239da214e3f
Tiago Ribeiro <tribeiro@lsst.org>
2025-10-02 09:49:22 -0700

    Add bash script to wait for the observing environment sidecar to be ready.

---
+++ build/common/wait_obsenv_sidecar.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# If running with obsenv sidecar, wait until the environment file exists.
+TIMEOUT=180
+
+if [[ -n "${OBSENV_SIDECAR:-}" ]]; then
+    echo "Running with obsenv sidecar. Waiting up to $TIMEOUT seconds for the environment file to be created..."
+
+    start=$(date +%s)
+
+    while [[ ! -e "${OBS_ENV_PATH}/auto_env_setup.sh" ]]; do
+        sleep 1
+        now=$(date +%s)
+        if (( now - start >= TIMEOUT )); then
+            echo "Timeout after $TIMEOUT seconds waiting for ${OBS_ENV_PATH}/auto_env_setup.sh"
+            exit 1
+        fi
+    done
+
+    echo "File '${OBS_ENV_PATH}/auto_env_setup.sh' has been created!"
+    git config --global --add safe.directory "${OBS_ENV_PATH}/*"
+else
+    echo "Obsenv sidecar disabled, continuing..."
+fi
+
+
