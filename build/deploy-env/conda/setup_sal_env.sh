0800ffc327c82fe9da560505725ac5c169b4f323
Tiago Ribeiro <tribeiro@lsst.org>
2025-10-14 14:20:39 -0700

    Update deploy-env conda base image to add support for the obsenv sidecar.

--- build/deploy-env/conda/setup_sal_env.sh
+++ build/deploy-env/conda/setup_sal_env.sh
@@ -11,28 +11,7 @@ term_handler() {
   exit 143; # 128 + 15 -- SIGTERM
 }
 
-# If running with obsenv sidecar, wait until the environment file exists.
-TIMEOUT=180
-${OBS_ENV_PATH}/auto_env_setup.sh
-
-if [[ -n "${OBSENV_SIDECAR:-}" ]]; then
-    echo "Running with obsenv sidecar. Waiting up to $TIMEOUT seconds for the environment file to be created..."
-
-    start=$(date +%s)
-
-    while [[ ! -e "${OBS_ENV_PATH}/auto_env_setup.sh" ]]; do
-        sleep 1
-        now=$(date +%s)
-        if (( now - start >= TIMEOUT )); then
-            echo "Timeout after $TIMEOUT seconds waiting for ${OBS_ENV_PATH}/auto_env_setup.sh"
-            exit 1
-        fi
-    done
-
-    echo "File '${OBS_ENV_PATH}/auto_env_setup.sh' has been created!"
-else
-    echo "Obsenv sidecar disabled, continuing..."
-fi
+${HOME}/.wait_obsenv_sidecar.sh
 
 # setup handlers
 # on callback, kill the last background process and execute term_handler
