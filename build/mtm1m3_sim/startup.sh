6ef2bc868a8bb09f1a4421e43696fd4da59c559a
Eric Coughlin <ecoughlin@lsst.org>
2026-04-14 13:56:34 -0400

    Fix a number of ld path calls in m1m3, m1m3ts and vms.

--- build/mtm1m3_sim/startup.sh
+++ build/mtm1m3_sim/startup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
 source ${HOME}/.setup_sal_env.sh
-export LD_LIBRARY_PATH=/usr/local/lib64/:/usr/local/lib:/opt/lsst/software/stack/miniconda/lib
+source ${HOME}/.sal_setup.sh
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CONDA_PREFIX/lib:/usr/local/lib64:/usr/local/lib"
+
+python ${HOME}/.create_topics_mtm1m3.py
 
 /home/saluser/ts_m1m3support/ts-M1M3supportd ${RUN_ARG} -c /home/saluser/ts_m1m3support/SettingFiles &
 
--- build/vms_sim/startup.sh
+++ build/vms_sim/startup.sh
@@ -2,7 +2,8 @@
 
 source ${HOME}/.setup_sal_env.sh
 source ${HOME}/.sal_setup.sh
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/lsst/ts_sal/lib/
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/lsst/ts_sal/lib/
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CONDA_PREFIX/lib:/usr/local/lib64:/usr/local/lib"
 
 python ${HOME}/.create_topics_vms.py
 
