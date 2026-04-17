7e6be7991ee8cb9c9096ef3f338df38f2168d00a
Petr Kubánek <petr.kubanek@noirlab.edu>
2026-04-17 12:19:57 +0200

    Modify develop-crio - remove RPMs, build everything from source.

--- build/deploy-crio/sal_setup.sh
+++ build/deploy-crio/sal_setup.sh
@@ -16,19 +16,20 @@ fi
 echo "LSST_SDK_INSTALL $LSST_SDK_INSTALL"
 
 export SAL_HOME=$LSST_SDK_INSTALL/lsstsal
-export SAL_WORK_DIR=$LSST_SDK_INSTALL/test
+export SAL_WORK_DIR=$LSST_SDK_INSTALL/
 
 export SAL_CPPFLAGS=-m64
-source $SAL_HOME/salenv.sh
+
+export CONDA_PREFIX=/opt/lsst/software/stack/miniconda/
+export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig
 
 if [ -z "$JAVA_HOME" ]; then
   export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 fi
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SAL_HOME}/lib
-export PATH=$JAVA_HOME/bin:${LSST_SDK_INSTALL}/bin:${PATH}
-
-export SAL_IDL_DIR=${SAL_WORK_DIR}/idl-templates/validated/sal
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib:$SAL_WORK_DIR/lib
+export LIBRARY_PATH=$SAL_WORK_DIR/lib
+export PATH=$JAVA_HOME/bin:$LSST_SDK_INSTALL/bin:$PATH
 
 export LSST_KAFKA_IP=`ip route get 1 | awk '{print $7;exit}'`
 export LSST_KAFKA_SCHEMA_REGISTRY=${LSST_SCHEMA_REGISTRY_URL}
@@ -44,8 +45,6 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SAL_WORK_DIR}/lib:${LSST_SDK_INSTALL
 export TS_SAL_DIR=$LSST_SDK_INSTALL
 export TS_XML_DIR=$LSST_SDK_INSTALL/../ts_xml
 export AVRO_RELEASE=`$SAL_DIR/avro_release.tcl`
-export AVRO_HOME=$SAL_HOME/lib
-export AVRO_INCL=$SAL_HOME/include/avro
 export AVRO_PREFIX=lsst.sal
 export AVRO_CLASSPATH=lsst/sal
 
