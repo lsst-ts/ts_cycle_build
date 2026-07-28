0e29461e9f3040b2770ae4fa2e27873266338de3
Eric Coughlin <eric.coughlin@noirlab.edu>
2026-07-28 15:08:45 -0500

    Use crio_setup.sh in m1m3ts_sim.

--- build/mtm1m3ts_sim/startup.sh
+++ build/mtm1m3ts_sim/startup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-source ${HOME}/.setup_sal_env.sh
-source ${HOME}/.sal_setup.sh
+source ${HOME}/.crio_setup.sh
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$CONDA_PREFIX/lib:/usr/local/lib64:/usr/local/lib"
 
 python ${HOME}/.create_topics_mtm1m3ts.py
