6e15bffe22afc5d09205ac826c3a6eb02e82ccb0
Eric Coughlin <eric.coughlin@noirlab.edu>
2026-07-30 09:58:02 -0500

    Adjust ordering of vms command invocation.

--- build/vms_sim/startup.sh
+++ build/vms_sim/startup.sh
@@ -4,4 +4,4 @@ source ${HOME}/.crio_setup.sh
 
 python ${HOME}/.create_topics_vms.py
 
-/home/saluser/ts_vms/ts-VMSd -c /home/saluser/ts_vms/MTVMS/v1 ${RUN_ARG} -f
+/home/saluser/ts_vms/ts-VMSd -c /home/saluser/ts_vms/SettingFiles/ -f ${RUN_ARG}
